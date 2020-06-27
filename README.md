eedb465e5f8d467af22ff536226b4703bee670d0
Ben Hilliard <benhilliard23@hotmail.com>
2020-06-27 09:31:43 +0930

    Commit of MVP however further additions to functionality remain

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Weather-Dashboard
 week 6 homework for Coding bootcamp
+
+I began by creating a basic HTML structure to reflect the example that we were provided with. I created .css and .js files and utlised bootstrap, jquery and moment.js to help with application functionality. I utlised a grid structure in HTML to assist with the layout.
+
+I spent a long time researching and understanding the different API's available on openweathermap. I eventually decided upon the API's and the queryURL's that I wanted to returned the output I required. 
+
+I constructed the an AJAX call to gather initial data and used this data to help construct the second AJAX call. A combination of the two AJAX calls helped me to get the data I required for the application. I utlised the learning assistants to assist with some questions regarding using multiple API's and AJAX calls.
+
+I created an onclick event to search for a city. ONce a city is searched, a record of this search is shown on the left hand side of the page.
+
+I utilised moment.js to show  the current date and the following 5 days dates.
+
+A screen shot is located below:
+Assets\Captureweatherdashboard.PNG
+
--- script.js
+++ script.js
@@ -2,89 +2,118 @@ $("document").ready(function () {
 
     //array containing city names
     var cities = [];
-    // This line grabs the city input from the search box
+    //dates for 5 day forecast
+    $("#day1").text(moment().add(1, 'd').format("MMM Do YYYY"));
+    $("#day2").text(moment().add(2, 'd').format("MMM Do YYYY"));
+    $("#day3").text(moment().add(3, 'd').format("MMM Do YYYY"));
+    $("#day4").text(moment().add(4, 'd').format("MMM Do YYYY"));
+    $("#day5").text(moment().add(5, 'd').format("MMM Do YYYY"));
 
     //This function handles what happens when button is clicked. Row add to left hand side table and weather data displayed//
     $("#submitBtn").on("click", function (event) {
         event.preventDefault();
         searchCity = $("#searchCity").val().trim();
+        //storage of last search
+        localStorage.setItem('city', searchCity);
+        document.querySelector("#searchCity").textContent = localStorage.getItem('city');
+        //URL for 1st AJAX call
         var queryUrl1 = "http://api.openweathermap.org/data/2.5/forecast?q=" + searchCity + "&units=metric&appid=dc93a678def19d88644d218aac7ed21c"
         console.log(queryUrl1)
         // Adding city from the search box to our array
         cities.push(searchCity);
-// running of functions
+        // running of functions
         renderTable();
         callAPI1(queryUrl1);
-        callAPI2(queryUrl2);
     })
 
     //call API function number 1
     function callAPI1(queryUrl1) {
+        //1st ajax call
         $.ajax({
             url: queryUrl1,
             method: "GET"
-        }).then(function (data) {
-          
-            console.log(data);
-            console.log(data.city.coord.lat)
-            console.log(data.city.coord.lon)
-            var latitude = data.city.coord.lat
-            var longitude = data.city.coord.lon
-            console.log(latitude)
-            var queryUrl2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "+&lon=" + longitude + "&units=metric&appid=dc93a678def19d88644d218aac7ed21c"
-
-        })
-        
-        function callAPI2 (queryUrl2) {
+        }).then(function (response1) {
+            console.log(response1);
+            console.log(response1.city.coord.lat);
+            console.log(response1.city.coord.lon);
+            var latitude = response1.city.coord.lat;
+            var longitude = response1.city.coord.lon;
+            console.log(latitude);
+            console.log(longitude);
+            var queryUrl2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&units=metric&appid=dc93a678def19d88644d218aac7ed21c";
+            $("#city").text(response1.city.name);
+            $("#country").text(response1.city.country);
+            $("#today").text(moment().format("MMM Do YYYY"));
+            console.log(moment().add(7, 'd').format("MMM Do YYYY"));
+            // 2nd ajax call
             $.ajax({
                 url: queryUrl2,
                 method: "GET"
-            }).then(function (data) {
-    
-                console.log(data);
-                //console.log(data.name)
-                // //$("#city").text(data.name)
-                // $("#country").text(data.sys.country)
-                // $("#temp").text(data.main.temp)
-                // $("#humid").text(data.main.humidity)
-                // $("#wind").text(data.wind.speed)
+            }).then(function (response2) {
+                var icon = (" src= openweathermap.org/img/wn/" + response2.current.weather[0].icon + "@2x.png>")
+                console.log(response2.current.weather[0].icon)
+                console.log(icon);
+                $("#iconCurrent").html(icon);
+                console.log(response2);
+                //for current conditions
+                $("#temp").text(response2.current.temp + "°C");
+                $("#humid").text(response2.current.humidity + "%");
+                $("#wind").text(response2.current.wind_speed + "km/h");
+                var uv = response2.current.uvi;
+                $("#uv").text(uv);
+
+                // uv index colours
+                if (uv <= 2) {
+                    $("#uv").addClass("green");
+                };
+                if ((uv >= 3) && (uv <= 5)) {
+                    $("#uv").addClass("gold");
+                };
+                if ((uv >= 6) && (uv <= 7)) {
+                    $("#uv").addClass("orange");
+                };
+                if ((uv >= 8) && (uv <= 10)) {
+                    $("#uv").addClass("red");
+                };
+                if (uv > 10) {
+                    $("#uv").addClass("purple");
+                };
+
+                // for 5 day forecast
+                $("#temp1").text(response2.daily[1].temp.max + "°C");
+                $("#temp2").text(response2.daily[2].temp.max + "°C")
+                $("#temp3").text(response2.daily[3].temp.max + "°C")
+                $("#temp4").text(response2.daily[4].temp.max + "°C")
+                $("#temp5").text(response2.daily[5].temp.max + "°C")
+                $("#hum1").text(response2.daily[1].humidity + "%")
+                $("#hum2").text(response2.daily[2].humidity + "%")
+                $("#hum3").text(response2.daily[3].humidity + "%")
+                $("#hum4").text(response2.daily[4].humidity + "%")
+                $("#hum5").text(response2.daily[5].humidity + "%")
+
             })
+        })
+    }
+
+    function clear() {
+        $(".forecast").empty();
+    }
+    // Function for saving prev searches on left hand side
+    function renderTable() {
+
+        // Deleting the cities prior to adding new cities (this is necessary otherwise will have repeat cities)
+        $("tbody").empty();
+
+        // Looping through the array of cities
+        for (var i = 0; i < cities.length; i++) {
+            // adding row to table for each city in the array
+            var a = $("<tr>");
+            // Adding a data-attribute
+            a.attr("data-name", cities[i]);
+            // adding the name of the city in the row
+            a.text(cities[i]);
+            // Adding the row to the tbody
+            $("tbody").prepend(a);
         }
     }
-    // function callAPI2(queryUrl2) {
-    //     $.ajax({
-    //         url: queryUrl2,
-    //         method: "GET"
-    //     }).then(function (data) {
-
-    //         console.log(data);
-    //         console.log(data.name)
-    //         $("#city").text(data.name)
-    //         $("#country").text(data.sys.country)
-    //         $("#temp").text(data.main.temp)
-    //         $("#humid").text(data.main.humidity)
-    //         $("#wind").text(data.wind.speed)
-    //     })
-    // }
-        function clear() {
-            $(".forecast").empty();
-        }
-        // Function for saving prev searches on left hand side
-        function renderTable() {
-
-            // Deleting the cities prior to adding new cities (this is necessary otherwise will have repeat cities)
-            $("tbody").empty();
-
-            // Looping through the array of cities
-            for (var i = 0; i < cities.length; i++) {
-                // adding row to table for each city in the array
-                var a = $("<tr>");
-                // Adding a data-attribute
-                a.attr("data-name", cities[i]);
-                // adding the name of the city in the row
-                a.text(cities[i]);
-                // Adding the row to the tbody
-                $("tbody").prepend(a);
-            }
-        }
-    });
+});
